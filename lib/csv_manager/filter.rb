f892651e0f81b03ddd276649f3c0190992ee687e
lukinvadim101 <71687637+lukinvadim101@users.noreply.github.com>
2022-05-30 18:02:18 -0500

    Update filter.rb

--- lib/csv_manager/filter.rb
+++ lib/csv_manager/filter.rb
@@ -1,22 +1,22 @@
 class Filter
-	def initialize(store_obj)
-		@store_obj = store_obj
-	end
+  def initialize(store_obj)
+    @store_obj = store_obj
+  end
 
   def execute(params)
-		field, action, condition = params
-		CSV::Table.new(public_send("filter_#{action}", field, condition), headers: :true)
-	end 
+    field, action, condition = params
+    CSV::Table.new(public_send("filter_#{action}", field, condition), headers: :true)
+  end 
 
-	def filter_more (field,condition)
-		@store_obj.store.select{|row| row[field].to_i > condition.to_i}
-	end
+  def filter_more (field,condition)
+    @store_obj.store.select{|row| row[field].to_i > condition.to_i}
+  end
 
-	def filter_less(field,condition)
-		@store_obj.store.select{|row| row[field].to_i < condition.to_i}
-	end
+  def filter_less(field,condition)
+    @store_obj.store.select{|row| row[field].to_i < condition.to_i}
+  end
 
-	def filter_equal(field,condition)
-		@store_obj.store.select{|row| row[field] == condition.to_s}
-	end
+  def filter_equal(field,condition)
+    @store_obj.store.select{|row| row[field] == condition.to_s}
+  end
 end

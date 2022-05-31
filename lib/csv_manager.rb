ec372e8efaf191c3ec5922f9d233289d46f38e57
lukinvadim101 <lukinvadim101@gmail.com>
2022-05-31 13:29:27 -0500

    m

--- lib/csv_manager.rb
+++ lib/csv_manager.rb
@@ -29,4 +29,4 @@ end
 
 # CsvManager.new().run(path: 'db/db.csv', filter:[['Age','more',10]])
 # CsvManager.new().run(path: 'db/db.csv', filter:[['Age','more',10],['Name','equal','Marge']])
-CsvManager.new.run(path: 'db/db.csv', sort: 'Id')
+# CsvManager.new.run(path: 'db/db.csv', sort: 'Id')
